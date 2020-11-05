dcc51f1a15e35d5dc3a775aa3ff86a810764ff4b
Kolowitz <bkolowitz@gmail.com>
2020-11-05 12:58:19 -0500

    move test data to folder & files

--- test/unit/atna2fhir.spec.js
+++ test/unit/atna2fhir.spec.js
@@ -1,23 +1,11 @@
-const converter = require('../../lib');
+const fs = require('fs');
 const moment = require('moment');
+const converter = require('../../lib');
+
 
 describe('Convert ATNA AuditMessage to FHIR AuditEvent', () => {
-    let rawAuditMessage =
-        `<AuditMessage>
-            <EventIdentification EventActionCode="E"
-                EventDateTime="2014-11-10T12:00:00.500-08:00" EventOutcomeIndicator="0">
-                <EventID csd-code="110100" codeSystemName="DCM" originalText="Application Activity"/>
-                <EventTypeCode csd-code="110120" codeSystemName="DCM" originalText="Application Start"/>
-            </EventIdentification>
-            <ActiveParticipant AlternativeUserID="alt@user"
-                NetworkAccessPointID="10.145.240.60"
-                NetworkAccessPointTypeCode="2" UserID="root" UserIsRequestor="false">
-                <RoleIDCode csd-code="110150" codeSystemName="DCM" originalText="Application"/>
-            </ActiveParticipant>
-            <AuditSourceIdentification code="4" AuditSourceID="10.0.0.1@ACCT"/>
-        </AuditMessage>`;
-
-    let expectedFhirAuditEvent = {"resourceType":"AuditEvent","text":{"status":"generated","div":"<div></div>"},"type":{"system":"http://dicom.nema.org/resources/ontology/DCM","code":"110100"},"period":"2014-11-10T12:00:00.500-08:00","recorded":"2020-11-05T15:59:00.019Z","outcome":"0","agent":[{"who":{"identifier":{"value":"root"}},"requestor":false,"network":{"address":"10.145.240.60","type":"2"}}],"source":{"observer":{"identifier":{"value":"10.0.0.1@ACCT"}}}};
+    let rawAuditMessage = fs.readFileSync('test/unit/data/atna.xml');
+    let expectedFhirAuditEvent = JSON.parse(fs.readFileSync('test/unit/data/fhir.json'));
 
     it('should return have a recorded time close to now()', async () => {
         const now = new Date();
