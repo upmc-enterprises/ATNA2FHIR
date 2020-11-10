d0c2e9a8c40270cdd0975b351fca06c0f15e66ec
Jeffrey Taylor <jeff@thetaylors.com>
2020-11-10 12:49:34 -0500

    fix jest & change .recorded test

--- test/unit/atna2fhir.spec.js
+++ test/unit/atna2fhir.spec.js
@@ -1,31 +1,31 @@
 const fs = require('fs')
-const moment = require('moment')
 const converter = require('../../lib')
 
 describe('Converts ATNA AuditMessages to FHIR AuditEvents', () => {
   describe('Converts ATNA AuditMessage Event Code "E" to FHIR AuditEvent', () => {
-    const rawAuditMessage = fs.readFileSync('test/unit/data/atna.xml')
     const expectedFhirAuditEvent = JSON.parse(fs.readFileSync('test/unit/data/fhir.json'))
-
-    it('should return have a recorded time close to now()', async () => {
-      const now = new Date()
-      const actualFhirAuditEvent = await converter.convert(rawAuditMessage)
-
-      // verify recorded is within N seconds of the test run
-      const diffInSeconds = moment(actualFhirAuditEvent.recorded).diff(moment(now), 'seconds')
-      const maxAllowedDiffInSeconds = 5
-      expect(diffInSeconds).toBeLessThanOrEqual(maxAllowedDiffInSeconds)
+    const rawAuditMessage = fs.readFileSync('test/unit/data/atna.xml')
+    const dateRightNowIsoString = 'mock iso string representation of date right now'
+    beforeEach(() => {
+      jest
+        .spyOn(global, 'Date')
+        .mockImplementation(() => {
+          return {
+            toISOString: () => dateRightNowIsoString
+          }
+        })
     })
 
-    it('should return convert ATNA AuditMessage to FHIR AuditEvent', async () => {
-      const actualFhirAuditEvent = await converter.convert(rawAuditMessage)
-      // check to see if a recorded time exists
-      expect(actualFhirAuditEvent.recorded).toBeDefined()
+    it('sets .recorded to the result of new Date().toISOString()', async () => {
+      const auditEvent = await converter.convert(rawAuditMessage)
+      expect(auditEvent.recorded).toEqual(dateRightNowIsoString)
+    })
 
-      // ignore the actual recorded time because it varies each run
-      actualFhirAuditEvent.recorded = expectedFhirAuditEvent.recorded
-
-      expect(actualFhirAuditEvent).toEqual(expectedFhirAuditEvent)
+    it('maps the ATNA message to FHIR AuditEvent', async () => {
+      const auditEvent = await converter.convert(rawAuditMessage)
+      // set the expected recordedDate time here, as it varies each run
+      expectedFhirAuditEvent.recorded = dateRightNowIsoString
+      expect(auditEvent).toEqual(expectedFhirAuditEvent)
     })
   })
 })
