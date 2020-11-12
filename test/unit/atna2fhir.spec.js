c6713138fed91bd2f9008cd1c072eb7388d0c52c
Kolowitz <bkolowitz@gmail.com>
2020-11-12 12:00:26 -0500

    merge main

--- test/unit/atna2fhir.spec.js
+++ test/unit/atna2fhir.spec.js
@@ -3,8 +3,8 @@ const converter = require('../../lib')
 
 describe('Converts ATNA AuditMessages to FHIR AuditEvents', () => {
   describe('Converts ATNA AuditMessage Event Code "E" to FHIR AuditEvent', () => {
-    const expectedFhirAuditEvent = JSON.parse(fs.readFileSync('test/unit/data/fhir.json'))
-    const rawAuditMessage = fs.readFileSync('test/unit/data/atna.xml')
+    const expectedFhirAuditEvent = JSON.parse(fs.readFileSync('test/unit/data/basic-fhir.json'))
+    const rawAuditMessage = fs.readFileSync('test/unit/data/basic-atna.xml')
     const dateRightNowIsoString = 'mock iso string representation of date right now'
     beforeEach(() => {
       jest
@@ -21,11 +21,21 @@ describe('Converts ATNA AuditMessages to FHIR AuditEvents', () => {
       expect(auditEvent.recorded).toEqual(dateRightNowIsoString)
     })
 
-    it('maps the ATNA message to FHIR AuditEvent', async () => {
+    it('maps the a basic ATNA AuditMessage to FHIR AuditEvent', async () => {
       const auditEvent = await converter.convert(rawAuditMessage)
       // set the expected recordedDate time here, as it varies each run
       expectedFhirAuditEvent.recorded = dateRightNowIsoString
       expect(auditEvent).toEqual(expectedFhirAuditEvent)
+    })
+
+    it('maps the an ATNA PDQ AuditMessage to FHIR AuditEvent', async () => {
+      const fhir = JSON.parse(fs.readFileSync('test/unit/data/pdq-fhir.json'))
+      const atna = fs.readFileSync('test/unit/data/pdq-atna.xml')
+
+      const auditEvent = await converter.convert(atna)
+      // set the expected recordedDate time here, as it varies each run
+      fhir.recorded = dateRightNowIsoString
+      expect(auditEvent).toEqual(fhir)
     })
   })
 })
